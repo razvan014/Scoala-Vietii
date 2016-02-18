1ff2960ac0061b66fa42e923aa73686edc226a6b
DELL <DELL>
2016-02-18 16:33:42 +0200

    Aurelian a adaugat equals in clasa Profesori

--- src/scoala/Profesori.java
+++ src/scoala/Profesori.java
@@ -11,4 +11,35 @@ public class Profesori {
 		this.materia = materia;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((materia == null) ? 0 : materia.hashCode());
+		result = prime * result + ((nume == null) ? 0 : nume.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Profesori other = (Profesori) obj;
+		if (materia == null) {
+			if (other.materia != null)
+				return false;
+		} else if (!materia.equals(other.materia))
+			return false;
+		if (nume == null) {
+			if (other.nume != null)
+				return false;
+		} else if (!nume.equals(other.nume))
+			return false;
+		return true;
+	}
+	
 }
